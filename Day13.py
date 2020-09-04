2c835880acbccfb7d43360acb95a89f749360539
pikuch <55052790+pikuch@users.noreply.github.com>
2020-09-04 15:33:39 +0200

    day 13 part 1

--- Day13.py
+++ Day13.py
@@ -7,6 +7,69 @@ def load_data(f_name):
     return data_read
 
 
-def run():
-    data = load_data("Day13.txt")
+def interpret(data):
+    carts = []
+    tracks = []
+    for row in range(len(data)):
+        current = list(data[row])
+        for col in range(len(current)):
+            if current[col] == "<":
+                carts.append([col, row, "W", 0])
+                current[col] = "-"
+            elif current[col] == ">":
+                carts.append([col, row, "E", 0])
+                current[col] = "-"
+            elif current[col] == "^":
+                carts.append([col, row, "N", 0])
+                current[col] = "|"
+            elif current[col] == "v":
+                carts.append([col, row, "S", 0])
+                current[col] = "|"
+        tracks.append(current)
+    return carts, tracks
 
+
+def move_cart(i, carts, tracks):
+    x_change = {"N": 0, "S": 0, "E": 1, "W": -1}
+    y_change = {"N": -1, "S": 1, "E": 0, "W": 0}
+    dir_change = {("N", "\\"): "W", ("N", "/"): "E",
+                  ("S", "\\"): "E", ("S", "/"): "W",
+                  ("E", "\\"): "S", ("E", "/"): "N",
+                  ("W", "\\"): "N", ("W", "/"): "S"}
+    cross_change = {("N", 0): "W", ("N", 1): "N", ("N", 2): "E",
+                    ("S", 0): "E", ("S", 1): "S", ("S", 2): "W",
+                    ("E", 0): "N", ("E", 1): "E", ("E", 2): "S",
+                    ("W", 0): "S", ("W", 1): "W", ("W", 2): "N"}
+    # move
+    carts[i][0] += x_change[carts[i][2]]
+    carts[i][1] += y_change[carts[i][2]]
+    # turn
+    track = tracks[carts[i][1]][carts[i][0]]
+    if track in "/\\":
+        carts[i][2] = dir_change[(carts[i][2], track)]
+    elif track == "+":
+        carts[i][2] = cross_change[(carts[i][2], carts[i][3])]
+        carts[i][3] = (carts[i][3] + 1) % 3
+
+
+def collided(i, carts):
+    for j in range(len(carts)):
+        if i != j:
+            if carts[i][0] == carts[j][0] and carts[i][1] == carts[j][1]:
+                return True
+    return False
+
+
+def simulate(carts, tracks):
+    while True:
+        carts.sort(key=lambda c: (c[1], c[0]))
+        for i in range(len(carts)):
+            move_cart(i, carts, tracks)
+            if collided(i, carts):
+                return carts[i][0], carts[i][1]
+
+
+def run():
+    data = load_data("Day13.txt").split("\n")
+    carts, tracks = interpret(data)
+    print(f"The first crash happens at {simulate(carts, tracks)}")
