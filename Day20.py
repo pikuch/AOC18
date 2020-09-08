199f18bb97123385a1b9316b1eec93c7cba51887
pikuch <55052790+pikuch@users.noreply.github.com>
2020-09-08 14:00:18 +0200

    test data

--- Day20.py
+++ Day20.py
@@ -9,4 +9,10 @@ def load_data(f_name):
 
 def run():
     data = load_data("Day20.txt")
+    # test data
+    data = "^WNE$"  # result == 3
+    # data = "^ENWWW(NEEE|SSE(EE|N))$"  # result == 10
+    # data = "^ENNWSWW(NEWS|)SSSEEN(WNSE|)EE(SWEN|)NNN$"  # result == 18
+    # data = "^ESSWWN(E|NNENN(EESS(WNSE|)SSS|WWWSSSSE(SW|NNNE)))$"  # result == 23
+    # data = "^WSSEESWWWNW(S|NENNEEEENN(ESSSSW(NWSW|SSEN)|WSWWN(E|WWS(E|SS))))$"  # result == 31
 
