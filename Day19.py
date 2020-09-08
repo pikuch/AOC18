b67ddbda73e859d8419058c65b43496d76021cbb
pikuch <55052790+pikuch@users.noreply.github.com>
2020-09-08 13:22:38 +0200

    day 19 done

--- Day19.py
+++ Day19.py
@@ -1,11 +1,20 @@
 # AOC18 day 19
 from theDeviceV2 import DeviceV2
+from math import sqrt, ceil
 
 
 def load_data(f_name):
     with open(f_name, "r") as f:
         data_read = f.read()
     return data_read
+
+
+def sum_of_divisors(n):
+    s = 0
+    for i in range(1, n+1):
+        if n % i == 0:
+            s += i
+    return s
 
 
 def run():
@@ -15,8 +24,12 @@ def run():
     # device.run()
     # print(f"Register 0 after the run: {device.reg[0]}")
 
-    device = DeviceV2()
-    device.load(data)
-    device.reg[0] = 1
-    for line in device.translate():
-        print(line)
+    # device = DeviceV2()
+    # device.load(data)
+    # device.reg[0] = 1
+    # for line in device.translate():
+    #     print(line)
+
+    # the device sums the divisors of 1017 (r0 = 0) or 10551417 (r0 = 1)
+    print(sum_of_divisors(1017))
+    print(sum_of_divisors(10551417))
