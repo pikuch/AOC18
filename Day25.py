a1af4ad6a5c2f7b055ae190d4d35326f756d4842
pikuch <55052790+pikuch@users.noreply.github.com>
2020-09-15 09:35:12 +0200

    day 25 test passed

--- Day25.py
+++ Day25.py
@@ -7,6 +7,49 @@ def load_data(f_name):
     return data_read
 
 
-def run():
-    data = load_data("Day25.txt")
+def parse_points(data):
+    points = []
+    lines = data.split("\n")
+    for line in lines:
+        points.append(tuple(map(int, line.split(","))))
+    return points
 
+
+def dist(p0, p1):
+    return abs(p1[0] - p0[0]) + abs(p1[1] - p0[1]) + abs(p1[2] - p0[2]) + abs(p1[3] - p0[3])
+
+
+def in_constellation(point, constellation):
+    for c_point in constellation:
+        if dist(point, c_point) <= 3:
+            return True
+    return False
+
+
+def make_constellations(points):
+    constellations = [[points[0]]]
+    for p in points[1:]:
+        part_of = []
+        for c in range(len(constellations)):
+            if in_constellation(p, constellations[c]):
+                part_of.append(c)
+        if len(part_of) == 0:
+            constellations.append([p])
+        elif len(part_of) == 1:
+            constellations[part_of[0]].append(p)
+        else:
+            constellations[part_of[0]].append(p)
+            for to_join in reversed(part_of[1:]):
+                constellations[part_of[0]].extend(constellations[to_join])
+                del constellations[to_join]
+
+    return constellations
+
+
+def run():
+    data = load_data("Day25test3.txt")
+    points = parse_points(data)
+    constellations = make_constellations(points)
+    print(f"There are {len(constellations)} constellations!")
+    for c in constellations:
+        print(c)
